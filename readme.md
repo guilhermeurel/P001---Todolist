8a8c8bf5dd8edce914988a5c0fff1aa4b2be84d4
Guilherme Hasse Urel <guilhermeurel@gmail.com>
2020-04-16 15:10:04 +0200

    Version 005: 16.04.2020

--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ Minimalistic project to develop a todolist with the purpose of development of fr
 
 - [x] Create open-source repository on github;
 - [x] Create readme file;
-- [ ] Create index.html and its main features;
+- [x] Create index.html and its main features;
 - [ ] Create style.css and its main features;
 - [ ] Create script.js and its main features;
 
--- todolist.js
+++ todolist.js
@@ -1,13 +1,19 @@
-var taskList = document.querySelector("#app ul");
-var taskInput = document.querySelector("#app input");
-var taskButton = document.querySelector("#app button");
-
+// 001 - Initializing variables from html
+var taskList = document.querySelector('#list ul');
+var buttons = document.querySelector('#buttons');
+var taskButton = document.createElement('button');
+taskButton.setAttribute('type', 'Text');
+taskButton.value = 'Add';
+var taskInput = document.createElement('input');
+taskInput.setAttribute('type', 'Text');
+taskInput.placeholder = 'New task';
+// 002 - Loading data from local storage or default
 var tasks = JSON.parse(localStorage.getItem('task_list')) || 'Example';
-
-
+// 003 - Functions
 function renderTasks() {
     taskList.innerHTML = '';
     for (task of tasks) {
+        // Creating task elements
         var itask = document.createElement('li');
         var itaskText = document.createTextNode(task);
         var iexclude = document.createElement('a');
@@ -15,15 +21,13 @@ function renderTasks() {
         var iexcludeText = document.createTextNode('Exclude');
         var pos = tasks.indexOf(task);
         iexclude.setAttribute('onclick', 'deleteTask(' + pos + ')');
-
+        // Creating task
         itask.appendChild(itaskText);
         taskList.appendChild(itask);
         iexclude.appendChild(iexcludeText);
         itask.appendChild(iexclude);
     }
 }
-
-renderTasks();
 
 function addTask() {
     var taskNew = taskInput.value;
@@ -32,7 +36,13 @@ function addTask() {
     taskInput.value = '';
     renderTasks();
     saveToStorage();
-}
+};
+
+function deleteTask(pos) {
+    tasks.splice(pos, 1);
+    renderTasks();
+    saveToStorage();
+};
 taskInput.addEventListener("keyup", function(event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
@@ -40,12 +50,10 @@ taskInput.addEventListener("keyup", function(event) {
     }
 });
 
-function deleteTask(pos) {
-    tasks.splice(pos, 1);
-    renderTasks();
-    saveToStorage();
-};
-
 function saveToStorage() {
     localStorage.setItem('task_list', JSON.stringify(tasks));
 };
+// 004 - Script
+renderTasks();
+buttons.appendChild(taskInput);
+buttons.appendChild(taskButton);
