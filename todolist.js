8174e225cd9549eaab87b1df0c144db7a5e9d0e2
Guilherme Hasse Urel <guilhermeurel@gmail.com>
2020-05-02 01:03:09 +0200

    Version 006: CSS

--- todolist.js
+++ todolist.js
@@ -1,59 +1,97 @@
-// 001 - Initializing variables from html
-var taskList = document.querySelector('#list ul');
-var buttons = document.querySelector('#buttons');
-var taskButton = document.createElement('button');
-taskButton.setAttribute('type', 'Text');
-taskButton.value = 'Add';
-var taskInput = document.createElement('input');
-taskInput.setAttribute('type', 'Text');
-taskInput.placeholder = 'New task';
-// 002 - Loading data from local storage or default
-var tasks = JSON.parse(localStorage.getItem('task_list')) || 'Example';
-// 003 - Functions
-function renderTasks() {
-    taskList.innerHTML = '';
-    for (task of tasks) {
-        // Creating task elements
-        var itask = document.createElement('li');
-        var itaskText = document.createTextNode(task);
-        var iexclude = document.createElement('a');
-        iexclude.setAttribute('href', '#');
-        var iexcludeText = document.createTextNode('Exclude');
-        var pos = tasks.indexOf(task);
-        iexclude.setAttribute('onclick', 'deleteTask(' + pos + ')');
-        // Creating task
-        itask.appendChild(itaskText);
-        taskList.appendChild(itask);
-        iexclude.appendChild(iexcludeText);
-        itask.appendChild(iexclude);
+    // 001 - Initializing variables from html
+    var form = document.getElementById('task_list');
+    var dform = document.getElementById('dtask_list');
+    //var taskButton = document.getElementById('addButton');
+    var clearButton = document.getElementById('clearButton');
+    var taskInput = document.querySelector('#buttons input');
+    // 002 - Loading data from local storage or default
+    var tasks = JSON.parse(localStorage.getItem('task_list')) || 'Example';
+    var dtasks = JSON.parse(localStorage.getItem('dtask_list')) || [];
+    // 003 - Functions
+    var br = document.createElement('br');
+    form.appendChild(br);
+
+    function renderTasks() {
+        form.innerHTML = '';
+        dform.innerHTML = '';
+        for (task of tasks) {
+            var currentTask = document.createElement('label');
+            currentTask.className = 'container';
+            currentTask.appendChild(document.createTextNode(task));
+            var currentTaskC = document.createElement('input');
+            currentTaskC.type = 'checkbox';
+            var currentTaskS = document.createElement('span');
+            currentTaskS.className = 'checkmark';
+            var pos = tasks.indexOf(task);
+            currentTaskC.setAttribute('onclick', 'setTimeout(deleteTask(' + pos + '), 3000)');
+            currentTask.appendChild(currentTaskC);
+            currentTask.appendChild(currentTaskS);
+            form.appendChild(currentTask);
+        }
+        if (dtasks && dtasks.length) {
+            for (task of dtasks) {
+                var currentTask = document.createElement('label');
+                currentTask.className = 'container';
+                currentTask.appendChild(document.createTextNode(task));
+                var currentTaskC = document.createElement('input');
+                currentTaskC.type = 'checkbox';
+                currentTaskC.checked = 'checked';
+                var currentTaskS = document.createElement('span');
+                currentTaskS.className = 'checkmark';
+                var pos = dtasks.indexOf(task);
+                currentTaskC.setAttribute('onclick', 'setTimeout(restoreTask(' + pos + '), 3000)');
+                currentTask.appendChild(currentTaskC);
+                currentTask.appendChild(currentTaskS);
+                dform.appendChild(currentTask);
+            }
+        }
     }
-}
 
-function addTask() {
-    var taskNew = taskInput.value;
+    function addTask() {
+        var taskNew = taskInput.value;
+        if (taskNew != '') {
+            tasks.push(taskNew);
+            taskInput.value = '';
+            renderTasks();
+            saveToStorage();
+        }
+    };
 
-    tasks.push(taskNew);
-    taskInput.value = '';
+    function clearTask() {
+        form.innerHTML = '';
+        dform.innerHTML = '';
+        tasks = [];
+        dtasks = [];
+        renderTasks();
+        saveToStorage();
+    };
+
+    function deleteTask(pos) {
+        dtasks.push(tasks[pos]);
+        tasks.splice(pos, 1);
+        renderTasks();
+        saveToStorage();
+    };
+
+    function restoreTask(pos) {
+        tasks.push(dtasks[pos]);
+        dtasks.splice(pos, 1);
+        renderTasks();
+        saveToStorage();
+    };
+
+    taskInput.addEventListener("keypress", function(event) {
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            addTask();
+        }
+    });
+
+    function saveToStorage() {
+        localStorage.setItem('task_list', JSON.stringify(tasks));
+        localStorage.setItem('dtask_list', JSON.stringify(dtasks));
+    };
+
     renderTasks();
-    saveToStorage();
-};
-
-function deleteTask(pos) {
-    tasks.splice(pos, 1);
-    renderTasks();
-    saveToStorage();
-};
-taskInput.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        addTask();
-    }
-});
-
-function saveToStorage() {
-    localStorage.setItem('task_list', JSON.stringify(tasks));
-};
-// 004 - Script
-renderTasks();
-buttons.appendChild(taskInput);
-buttons.appendChild(taskButton);
+    //taskButton.onclick = addTask;
+    clearButton.onclick = clearTask;
